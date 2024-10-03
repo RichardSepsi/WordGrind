2e13771f23fc038c92074a07d9a0794a294a5ee1
Richard Sepši <appsepsi@gmail.com>
2024-10-03 17:24:08 +0200

    deprecated json file

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-var deckdata;
 
-fetch ("data.json")
+var rawjson = `
+{
+    "漢字単語" : {
+        "card0" : {
+            "front" : "簡単",
+            "answer" : "かんたん",
+            "back" : "simple, easy, uncomplicated",
+            "hint" : "no hint :/"
+        },
+        "card1" : {
+            "front" : "向かう",
+            "answer" : "むかう",
+            "back" : "to face, to go towards, to head towards",
+            "hint" : "no hint :/"
+        },
+        "card2" : {
+            "front" : "終わる",
+            "answer" : "おわる",
+            "back" : "to end, to come to an end, to finish, to close, to be over",
+            "hint" : "no hint :/"
+        },
+        "card3" : {
+            "front" : "体",
+            "answer" : "からだ",
+            "back" : "body",
+            "hint" : "no hint :/"
+        },
+        "card4" : {
+            "front" : "音",
+            "answer" : "おと",
+            "back" : "sound, noise",
+            "hint" : "no hint :/"
+        }
+    }
+}
+`
+
+var deckdata = JSON.parse(rawjson);
+
+/*fetch ("data.json")
     .then(response => response.json())
     .then(deckdt => deckdata = deckdt)
     .then(data => {
@@ -13,4 +51,4 @@ fetch ("data.json")
             }
         };
 
-    })
+    })*/
