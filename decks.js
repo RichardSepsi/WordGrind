f6f65dc6dbbfb2ad019ca1a3bee3ec1ec98141fc
Richard Sepši <appsepsi@gmail.com>
2024-10-03 21:19:51 +0200

    finished core functionality

---
+++ decks.js
@@ -0,0 +1,55 @@
+if(Object.keys(deckdata).length < 2){
+    document.getElementById("expand-button").style.opacity = "25%"
+    document.getElementById("selector-wrapper").style.pointerEvents = "none"
+}
+if(Object.keys(deckdata).length > 4){
+    document.getElementById("deck-options-container").style.width = "410px"
+    document.getElementById("deck-options-container").style.right = "calc((100% - (399px / 1)) + (-4px))"
+}
+
+let counter = 0
+
+Object.keys(deckdata).forEach(key => {
+    if(counter == 0){
+        document.getElementById("id-deck-name").innerHTML = key
+    }
+    else {
+        document.getElementById("deck-options-container").innerHTML += `
+        <div class="home-selector-container-option" id="opt-`+(counter+1)+`" onclick="setdecks(this.id)">
+            <span style="font-size: 28px; font-weight: 600; color: #969696; padding-left: 23px; pointer-events: none;" id="opt-`+(counter+1)+`-name">`+key+`</span>
+            <span style="font-size: 20px; font-weight: 600; color: #696969; padding-bottom: 1px; padding-left: 23px; pointer-events: none;" id="opt-`+(counter+1)+`-num">#0`+(counter+1)+`</span>
+        </div>
+        `
+    }
+    console.log(key);
+    counter = counter + 1
+});
+
+
+function expanddecks(){
+    document.getElementById("deck-options-container").style.opacity = "1"
+    document.getElementById("deck-options-container").style.pointerEvents = "all"
+    document.getElementById("arrow-down-img").style.rotate = "180deg"
+    document.getElementById("arrow-down-img").style.marginTop = "0px"
+    document.getElementById("arrow-down-img").style.marginBottom = "2px"
+    document.getElementById("home-selector-container").setAttribute("onClick", "closedecks()")
+}
+
+function setdecks(id1){
+    let olddeck = document.getElementById("id-deck-name").innerHTML
+    let olddecknum = document.getElementById("id-deck-num").innerHTML
+    document.getElementById("id-deck-name").innerHTML =  document.getElementById(id1+"-name").innerHTML
+    document.getElementById("id-deck-num").innerHTML =  document.getElementById(id1+"-num").innerHTML
+    document.getElementById(id1+"-name").innerHTML = olddeck
+    document.getElementById(id1+"-num").innerHTML = olddecknum
+    closedecks()
+}
+
+function closedecks(){
+    document.getElementById("deck-options-container").style.opacity = "0"
+    document.getElementById("deck-options-container").style.pointerEvents = "none"
+    document.getElementById("arrow-down-img").style.rotate = "0deg"
+    document.getElementById("arrow-down-img").style.marginTop = "2px"
+    document.getElementById("arrow-down-img").style.marginBottom = "0px"
+    document.getElementById("home-selector-container").setAttribute("onClick", "expanddecks()")
+}
--- script.js
+++ script.js
@@ -32,6 +32,134 @@ var rawjson = `
             "back" : "sound, noise",
             "hint" : "no hint :/"
         }
+    },
+    "ひらがな単語" : {
+        "card0" : {
+            "front" : "ずっと",
+            "answer" : "continuously",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card1" : {
+            "front" : "こちら",
+            "answer" : "this way",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card2" : {
+            "front" : "くらい",
+            "answer" : "approximately",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card3" : {
+            "front" : "ただ (not free)",
+            "answer" : "simply",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card4" : {
+            "front" : "これから",
+            "answer" : "from now on",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card5" : {
+            "front" : "くれる",
+            "answer" : "to give",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card6" : {
+            "front" : "もう",
+            "answer" : "already",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card7" : {
+            "front" : "いつ",
+            "answer" : "when",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card8" : {
+            "front" : "また",
+            "answer" : "again",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card9" : {
+            "front" : "つもり",
+            "answer" : "intention",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card10" : {
+            "front" : "どうして",
+            "answer" : "why",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card11" : {
+            "front" : "まだ",
+            "answer" : "yet",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card12" : {
+            "front" : "いつも",
+            "answer" : "always",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card13" : {
+            "front" : "ちゃんと",
+            "answer" : "properly",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card14" : {
+            "front" : "ため",
+            "answer" : "sake",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card15" : {
+            "front" : "おかしい",
+            "answer" : "strange",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card16" : {
+            "front" : "どんな",
+            "answer" : "what kind of",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card17" : {
+            "front" : "こう",
+            "answer" : "like this",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card18" : {
+            "front" : "もし",
+            "answer" : "if",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card19" : {
+            "front" : "さっき",
+            "answer" : "a moment ago",
+            "back" : "-",
+            "hint" : "no hint :/"
+        },
+        "card20" : {
+            "front" : "うち",
+            "answer" : "one's household",
+            "back" : "-",
+            "hint" : "no hint :/"
+        }
     }
 }
 `
